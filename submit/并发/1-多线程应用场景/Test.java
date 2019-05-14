a974055ecc94fce7d27fd79548b0fb2d131cc57c
jiangdi <957133710@qq.com>
2019-05-14 22:19:24 +0800

    concurrency

--- submit/并发/1-多线程应用场景/Test.java
+++ submit/并发/1-多线程应用场景/Test.java
@@ -1,10 +1,4 @@
-package com.str818.exercise.demo1;
-
-import java.sql.SQLOutput;
-import java.util.Random;
-
 public class Process {
-
     public static void main(String[] args) {
 
         double[] data = new double[100000000];
@@ -12,31 +6,31 @@ public class Process {
         long startTime = System.currentTimeMillis();
         process(data);
         long endTime = System.currentTimeMillis();
-        System.out.println("不使用多线程耗时：" + ((endTime - startTime) / 1000) + "s" );
+        System.out.println("不使用多线程耗时：" + ((endTime - startTime) / 1000) + "s");
 
         startTime = System.currentTimeMillis();
         processUseThread(data);
         endTime = System.currentTimeMillis();
-        System.out.println("使用多线程耗时：" + ((endTime - startTime) / 1000) + "s" );
+        System.out.println("使用多线程耗时：" + ((endTime - startTime) / 1000) + "s");
 
     }
 
-    public static void process(double[] data){
-        for(int i = 0; i < data.length; i++){
+    public static void process(double[] data) {
+        for (int i = 0; i < data.length; i++) {
             data[i] += Math.sqrt(Math.random() * Math.random() * Math.random());
         }
     }
 
-    public static void processUseThread(double[] data){
+    public static void processUseThread(double[] data) {
 
         new Thread(() -> {
-            for(int i = 0; i < data.length / 2; i++){
+            for (int i = 0; i < data.length / 2; i++) {
                 data[i] += Math.sqrt(Math.random() * Math.random() * Math.random());
             }
         }).start();
 
         new Thread(() -> {
-            for(int i = data.length / 2; i < data.length; i++){
+            for (int i = data.length / 2; i < data.length; i++) {
                 data[i] += Math.sqrt(Math.random() * Math.random() * Math.random());
             }
         }).start();
